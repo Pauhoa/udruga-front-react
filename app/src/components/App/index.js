2f399e357d91d7d49777f012ae912b864f3e9a29
Pauhoa <hoarau.paul3@gmail.com>
2022-05-04 14:50:33 +0200

    clean starting

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -3,19 +3,7 @@ import './app.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Coucou, bienvenue sur Udruga</p>
     </div>
   );
 }
