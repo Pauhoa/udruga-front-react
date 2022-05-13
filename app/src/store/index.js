78bca5ca822dffbbcfc32b91d1f198031cea0d12
laurentnunez <lnunez@hotmail.fr>
2022-05-13 12:42:21 +0200

    signinMiddleware added on store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -3,8 +3,9 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import loginMiddleware from '../middlewares/login';
 import createEventMiddleware from '../middlewares/createEvent';
+import signinMiddleware from '../middlewares/signin';
 
-const middlewares = [loginMiddleware, createEventMiddleware];
+const middlewares = [loginMiddleware, createEventMiddleware, signinMiddleware];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
