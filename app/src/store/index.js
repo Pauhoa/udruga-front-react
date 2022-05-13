1b1a79189c2c1f000e9711a7de04b940c4d72348
laurentnunez <lnunez@hotmail.fr>
2022-05-13 12:48:24 +0200

    store debug

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -3,10 +3,6 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import loginMiddleware from '../middlewares/login';
 
-import createEventMiddleware from '../middlewares/createEvent';
-
-const middlewares = [loginMiddleware, signinMiddleware, createEventMiddleware];
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
