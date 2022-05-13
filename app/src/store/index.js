706443b4de59fed1d76197c82fcded81836cda15
UkinaMorohiko <jeremy.lefevre78@gmail.com>
2022-05-13 15:19:33 +0200

    debug-store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -3,7 +3,7 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import loginMiddleware from '../middlewares/login';
 import signinMiddleware from '../middlewares/signin';
-import createEventMiddleware from '../middlewares/createEventMiddleware';
+import createEventMiddleware from '../middlewares/createEvent';
 
 const middlewares = [loginMiddleware, signinMiddleware, createEventMiddleware];
 
