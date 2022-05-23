4fe94feb37cc6bbbe27fcc6a74e5c0e6cc4d649b
Pauhoa <hoarau.paul3@gmail.com>
2022-05-23 14:30:41 +0200

    cleaning css and test states, consoles logs

--- app/src/components/Team/index.js
+++ app/src/components/Team/index.js
@@ -5,8 +5,6 @@ import { ReactComponent as LinkedinSvg } from '../../assets/linkedin.svg';
 
 function Team() {
   return (
-    // todo RECUPERER SVG DE CHACUN
-    // todo RECUPERER LIEN GITHUB ET LINKEDIN
     <div className="team">
       <div>
         <h2>L'équipe Front-End</h2>
@@ -34,31 +32,15 @@ function Team() {
           <div className="team-member__infos">
             <h3>Laurent Nunez</h3>
             <p>Développeur front-end, product owner</p>
-            <div className="links">
-              <a href="#">
-                <GithubSvg href="#" className="link link-github" />
-              </a>
-              <a href="https://www.linkedin.com/in/paulhoarau/">
-                <LinkedinSvg href="https://www.linkedin.com/in/paulhoarau/" className="link link-linkedin" />
-              </a>
-            </div>
           </div>
         </div>
         <div className="team-member">
           <div className="team-member__image">
-            <img src="https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Blank&facialHairType=BeardMajestic&facialHairColor=Black&clotheType=Hoodie&clotheColor=Blue01&eyeType=Default&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Light" alt="member avatar" />
+            <img alt="member avatar" src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairBun&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Side&eyebrowType=UpDown&mouthType=Default&skinColor=Pale" />
           </div>
           <div className="team-member__infos">
-            <h3>Nom prénom</h3>
-            <p>Courte description</p>
-            <div className="links">
-              <a href="#">
-                <GithubSvg href="#" className="link link-github" />
-              </a>
-              <a href="https://www.linkedin.com/in/paulhoarau/">
-                <LinkedinSvg href="https://www.linkedin.com/in/paulhoarau/" className="link link-linkedin" />
-              </a>
-            </div>
+            <h3>Jérémy Lefèvre</h3>
+            <p>Développeur front-end, scrum master</p>
           </div>
         </div>
         <h2>L'équipe Back-End</h2>
